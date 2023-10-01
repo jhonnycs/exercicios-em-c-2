439b3b1151df89ae9698d0f81a1a11f555d6d2eb
jhonnycs <jhonnycunha7@gmail.com>
2023-10-01 16:23:23 -0300

    correção questão 5

--- questao05.c
+++ questao05.c
@@ -8,11 +8,16 @@ int main() {
 
     if (n % 10 == 0) {
         printf("\nO número %d é divisível por 10", n);
-    } else if (n % 5 == 0) {
+    }
+    if (n % 5 == 0) {
         printf("\nO número %d é divisível por 5", n);
-    } else if (n % 2 == 0) {
+    }
+    if (n % 2 == 0) {
         printf("\nO número %d é divisível por 2", n);
-    } else {
+    }
+    
+
+    if (n % 10 != 0 && n % 5 != 0 && n % 2 != 0) {
         printf("\nO número %d não é divisível por nenhuma das opções (2, 5, 10)", n);
     }
 
